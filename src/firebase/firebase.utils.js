4e7ad901897c44078aa466306188dd5c4ec62105
amshannon <andrewmartinshannon@gmail.com>
2020-08-11 12:22:24 +0100

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,15 +3,44 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyD3zk_h_hNw3tJisO9xhOsaOO3M5RPvef8",
-    authDomain: "crwn-clothing-db-88ca1.firebaseapp.com",
-    databaseURL: "https://crwn-clothing-db-88ca1.firebaseio.com",
-    projectId: "crwn-clothing-db-88ca1",
-    storageBucket: "crwn-clothing-db-88ca1.appspot.com",
-    messagingSenderId: "535647484944",
-    appId: "1:535647484944:web:46df209bcac2dc88ed5e95",
-    measurementId: "G-7TFFVHK94D"
-  };
+  apiKey: "AIzaSyD3zk_h_hNw3tJisO9xhOsaOO3M5RPvef8",
+  authDomain: "crwn-clothing-db-88ca1.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-db-88ca1.firebaseio.com",
+  projectId: "crwn-clothing-db-88ca1",
+  storageBucket: "crwn-clothing-db-88ca1.appspot.com",
+  messagingSenderId: "535647484944",
+  appId: "1:535647484944:web:46df209bcac2dc88ed5e95",
+  measurementId: "G-7TFFVHK94D"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  
+  if(!userAuth) return; // no userAuth returned i.e. signing out
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get()
+
+  if(!snapShot.exists) {
+    // If doesn't already exit then create
+    const { displayName, email } = userAuth;
+    const createdAt = new Date(); // new js date obj to tell us current date/time
+    try {
+      await userRef.set ({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+
+  }
+
+  return userRef;
+
+}
 
 firebase.initializeApp(config);
 
