42dd51e10d30ad45b62efcc5690b2630ddfddf98
amshannon <andrewmartinshannon@gmail.com>
2020-08-19 12:16:43 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
     // no need to pass payload as this is optional and not used
 })
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,19 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+
+    console.log("addItemToCart")
+
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+
+    if(existingCartItem) {
+        console.log("existingCartItem FOUND")
+        return cartItems.map(cartItem => 
+            cartItem.id == cartItemToAdd.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }    
+            : cartItem
+        )
+    } else {console.log("existingCartItem NOT FOUND")}
+
+    return [ ...cartItems, { ...cartItemToAdd, quantity: 1 }];
+}
